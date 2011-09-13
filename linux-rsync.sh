1fab56591a64121f70da26c2c4f242dc2e9f956b
lumixia <win.milan@gmail.com>
2011-09-13 13:10:17 +0800

    Modify linux-rsync.sh

--- linux-rsync.sh
+++ linux-rsync.sh
@@ -1,7 +1,20 @@
 #bin/bash
 
 echo "[KAKA]    该文件是一个同步各个路径下配置文件到该目录下的脚本"
-echo "[KAKA]    "`pwd`
+echo "[KAKA]    "`pwd`  $HOME
 
+CURRENT=`pwd`
+
+echo "--------------------------------------------"
+echo "[SYNC]    "$HOME
+rsync -r -t -p -v --progress --existing -s $HOME/.config/ $CURRENT/home/kaka/.config/
+rsync -r -t -p -v --progress --existing -s $HOME/.vim/ $CURRENT/home/kaka/.vim/
+
+rsync -r -t -p -v --progress -s $HOME/.bashrc $CURRENT/home/kaka/.bashrc
+
+echo "[SYNC]    /etc/"
+rsync -r -t -p -v --progress --existing -s /etc/ $CURRENT/etc/
+#rsync -r -t -p -v --progress --existing -s $HOME/.config/ $CURRENT/home/kaka/.config/
+echo "--------------------------------------------"
 
 
